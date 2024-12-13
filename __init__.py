8305b58d7561dbd847e0d850954e76dcecb77299
DKchel <kuda74ru@gmail.com>
2024-12-13 19:11:09 +0500

    initial

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from Seminar8.task1 import text_to_json
+from Seminar8.task2 import access_users
+from Seminar8.task3 import json_to_csv
+from Seminar8.task4 import csv2json
+from Seminar8.task5 import json_to_pickle
+from Seminar8.task6 import pickle2csv
+from Seminar8.task7 import csv2pickles
---
+++ main.py
@@ -0,0 +1,16 @@
+from Seminar8 import text_to_json
+from Seminar8 import access_users
+from Seminar8 import json_to_csv
+from Seminar8 import csv2json
+from Seminar8 import json_to_pickle
+from Seminar8 import ickle2csv
+from Seminar8 import ickle2csv
+
+if __name__ == '__main__':
+    text_to_json()
+    access_users()
+    json_to_csv()
+    csv2json()
+    json_to_pickle()
+    ickle2csv()
+    sv2pickles()
